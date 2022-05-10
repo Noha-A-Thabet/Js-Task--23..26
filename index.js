eac5dbeb54f4c0f7820cde1115dec6c51e0809d4
Noha-A-Thabet <NohaAshraf25@outlook.com>
2022-05-10 06:10:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Task 1 
+console.log(100_000);
+console.log(100000);
+console.log(1e5);
+console.log(5e4 + 5e4);
+console.log(Math.pow(10, 5));
+
+
+// Task 2 
+console.log(-(Number.MIN_SAFE_INTEGER));
+
+// TASK 3
+console.log(Number.MAX_SAFE_INTEGER.toString())
+console.log(Number.MAX_SAFE_INTEGER.toString().length)
+
+// Task 4
+let myVar = "100.56789 Views";
+console.log(parseInt(myVar));
+console.log(+(parseFloat(myVar).toFixed(2)));
+
+// Task 5 
+let num = 10;
+console.log(num.toString().length);
+
+// Task 6 
+let flt = 10.4;
+
+console.log(+(flt.toFixed()));
+console.log(parseInt(flt));
+console.log(Math.floor(flt));
+console.log(Math.round(flt));
+console.log(Math.trunc(flt))
+
+
+// Task 7 
+console.log(parseInt(Math.random() * 4) + 1);
